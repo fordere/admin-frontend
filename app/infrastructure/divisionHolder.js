5d15fc4b02176dd78af414e39fd4d08d5b9db8f7
Stefan Schöb <opendix@gmail.com>
2020-03-21 19:20:38 +0100

    cleanup progress

--- app/infrastructure/divisionHolder.js
+++ app/infrastructure/divisionHolder.js
@@ -1,68 +1,22 @@
 ﻿(function () {
 
-    angular.module('fordere.services').service('divisionHolder', ['$location', divisionHolder]);
+    angular.module('fordere.services').service('divisionHolder', [divisionHolder]);
 
-    function divisionHolder($location) {
+    function divisionHolder() {
         var vm = this;
         vm.select = select;
         vm.getSelectedDivision = getSelectedDivision;
 
         function select(divisionId) {
-            document.cookie = 'divisionId=' + divisionId + ';domain=fordere.ch';
-
-            if (!window.location.host.startsWith('127.0.0.1')) {
-                setTimeout(() => {
-                    switch (divisionId) {
-                        case 1:
-                            window.location.href = 'https://zh.fordere.ch' + location.pathname;
-                            break;
-                        case 3:
-                            window.location.href = 'https://lu.fordere.ch' + location.pathname;
-                            break;
-                        case 2:
-                            window.location.href = 'https://sg.fordere.ch' + location.pathname;
-                            break;
-                        case 4:
-                            window.location.href = 'https://winti.fordere.ch' + location.pathname;
-                            break;
-                    }
-                });
-            }
+            localStorage.setItem('divisionId', divisionId);
         }
 
         function getSelectedDivision() {
             return getCurrentDivision();
         }
 
-        function getCookie(name) {
-            var value = "; " + document.cookie;
-            var parts = value.split("; " + name + "=");
-            if (parts.length === 2) return parts.pop().split(";").shift();
-        }
-
         function getCurrentDivision() {
-
-            if (window.location.host.startsWith('127.0.0.1')) {
-                return 1;
-            }
-
-            var parts = window.location.host.split('.');
-            if (parts[0] !== 'fordere') {
-                switch (parts[0]) {
-                    case 'zh':
-                        return 1;
-                    case 'lu':
-                    case 'luzern':
-                        return 3;
-                    case 'sg':
-                        return 2;
-                    case 'winti':
-                        return 4;
-
-                }
-            }
-
-            var divisonId = Number(getCookie('divisionId'));
+            var divisonId = Number(localStorage.getItem('divisionId'));
 
             if (Number.isNaN(divisonId)) {
                 return undefined;
@@ -72,19 +26,9 @@
         }
 
         if (getCurrentDivision() === undefined) {
-
-            if (window.location.host.startsWith('127.0.0.1')) {
-                this.select(1);
-            } else {
-                $location.path('/divisionselection');
-            }
-
+            this.select(1);
         } else {
-
-            var parts = window.location.host.split('.');
-            if (parts[0] === 'fordere') {
-                select(getCurrentDivision());
-            }
+            select(getCurrentDivision());
         }
 
     }
