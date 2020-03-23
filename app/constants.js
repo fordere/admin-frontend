a2cc0c63059965108080e0a043f1bd126aeb6492
Stefan Schöb <opendix@gmail.com>
2020-03-23 07:47:07 +0100

    wip

--- app/constants.js
+++ app/constants.js
@@ -5,6 +5,6 @@
 
         .constant('DATETIMEFORMAT', 'dd.MM.yy HH:mm')
         .constant('DATEFORMAT', 'dd.MM.yy')
-        .constant('API_URL', document.domain.indexOf('fordere.ch') > -1 ? 'https://' + document.domain + '/api/v1/' : 'http://127.0.0.1:1337/');
+        .constant('API_URL', document.domain.indexOf('fordere.ch') > -1 ? '/api/v1/' : '/api/');
 
 })();
--- app/ui/layout/headerCtrl.js
+++ app/ui/layout/headerCtrl.js
@@ -6,26 +6,12 @@
     angular.module('fordere')
         .controller(controllerId, ['AuthService', 'seasonService', '$window', 'divisionHolder', 'competitionService', menuCtrl]);
 
-    function menuCtrl(authService, seasonService, $window, divisionHolder, competitionService) {
+    function menuCtrl(authService, seasonService, $window, divisionHolder) {
         var vm = this;
 
         vm.isAdmin = authService.isAdmin;
-
-        vm.shouldShowMySeason = shouldShowMySeason;
-        vm.shouldShowRegister = shouldShowRegister;
-        vm.shouldShowStandings = shouldShowStandings;
-        vm.shouldShowRegisterNow = shouldShowRegisterNow;
         vm.reload = reload;
         vm.isDivision = isDivision;
-        vm.isFinalDay = isFinalDay;
-
-        function isFinalDay() {
-            if (vm.season) {
-                return vm.season.State === 'FinalDay';
-            }
-
-            return false;
-        }
 
         function isDivision(divisionId) {
             return divisionHolder.getSelectedDivision() === divisionId;
@@ -36,49 +22,6 @@
                 $window.location.reload();
             }
         }
-
-        function shouldShowRegisterNow() {
-            if (vm.season) {
-                return vm.season.State === 'Registration';
-            }
-            return false;
-        }
-
-        function shouldShowMySeason() {
-            if (vm.season) {
-                return vm.season.State === 'Running';
-            }
-            return false;
-        }
-
-        function shouldShowRegister() {
-            if (vm.season) {
-                return vm.season.State === 'Registration' || vm.season.State === 'PrepareSeason';
-            }
-            return false;
-        }
-
-        function shouldShowStandings() {
-            if (vm.season) {
-                return vm.season.State === 'Running' || vm.season.State === 'PrepareFinalDay' || vm.season.State === 'FinalDay';
-            }
-
-            return false;
-        }
-
-        function init() {
-            seasonService.getCurrent().$promise.then(season => {
-                vm.season = season;
-
-                if (vm.season.Id) {
-                    competitionService.getBySeasonId({ Id: season.Id }).$promise.then(competitions => {
-                        vm.competitions = competitions;
-                    });
-                }
-            });
-        }
-
-        init();
     }
 
 })();
